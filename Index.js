c1bf722ebe0bea0d954f3cf4435459683b15895e
Alex Handley <smithvillescholarsah@gmail.com>
2025-02-20 17:34:39 -0600

    set up express server

--- Index.js
+++ Index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+const express = require("express");
+const path = require("path");
+const server = express();
+const port = 5500;
+
+server.use(express.static(path.join(__dirname, "src")));
+
+server.get("/", function(request, response) {{
+    response.sendFile(path.join(__dirname, "src", "index.html"));
+}});
+
+server.listen(port, function() {
+    console.log(`*** Website is live on: http://localhost:${port} ***`);
+});
