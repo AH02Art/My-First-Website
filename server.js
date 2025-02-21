252c43848189cc3e13e0fd556c2e6eddb3d36dcd
Alex Handley <smithvillescholarsah@gmail.com>
2025-02-21 16:04:02 -0600

    set up live browser refresh with nodemon

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const livereload = require("livereload");
+const connectLivereload = require("connect-livereload");
+// these are my testing and work dependencies
+const server = express();
+const port = 5500;
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "src"));
+server.use(connectLivereload());
+
+server.use(express.static(path.join(__dirname, "src")));
+
+server.get("/", function(request, response) {{
+    response.sendFile(path.join(__dirname, "src", "index.html"));
+}});
+
+server.listen(port, function() {
+    console.log(`*** Website is live on: http://localhost:${port} ***`);
+});
+
+liveReloadServer.server.once("connection", function() {
+    setTimeout(function() {
+        liveReloadServer.refresh("/");
+    }, 100);
+})
