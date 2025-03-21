bedf8fa5168080b85774aca1d66fad8c1452515e
Alex Handley <smithvillescholarsah@gmail.com>
2025-03-20 19:54:13 -0500

    project updated to ejs

--- server.js
+++ server.js
@@ -1,27 +1,52 @@
+// server dependencies
 const express = require("express");
 const path = require("path");
+// These dependencies are to use Live Server + nodemon together
 const livereload = require("livereload");
 const connectLivereload = require("connect-livereload");
 
-const server = express();
-const port = 5500;
+const app = express();
+const PORT = 5500;
 
+// what specified folders to monitor changes
 const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, "src"));
-server.use(connectLivereload());
+liveReloadServer.watch([
+    path.join(__dirname, "public"),
+    path.join(__dirname, "views")
+]);
 
-server.use(express.static(path.join(__dirname, "src")));
-
-server.get("*", function(request, response) {{
-    response.sendFile(path.join(__dirname, "src", "index.html"));
-}});
-
-server.listen(port, function() {
-    console.log(`*** Website is live on: http://localhost:${port} ***`);
-});
-
+// a Timer for when the server resets after changes (I may change it...)
 liveReloadServer.server.once("connection", function() {
+    console.log("LiveReload online");
     setTimeout(function() {
+        console.log("Browser refreshed...");
         liveReloadServer.refresh("/");
     }, 100);
+});
+
+// I commented this stuff out because it's not needed, but I'll save for reference...
+/* Live server displaying what file got changed
+liveReloadServer.server.on("change", (file) => {
+    if (file.endsWith(".ejs")) {
+        console.log(`File updated: ${file}`);
+        liveReloadServer.refresh("/"); 
+    }
+});
+*/
+
+app.use(connectLivereload());
+app.use(express.static(path.join(__dirname, "public")));
+
+// setting ejs as the template engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Some routes
+app.get("/", (request, response) => response.render("index", { title: "Home" }));
+app.get("/about", (request, response) => response.render("about", { title: "About" }));
+app.get("/contact", (request, response) => response.render("contact", { title: "Contact" }));
+
+// Starting the server
+app.listen(PORT, function() {
+    console.log(`*** Website is live on: http://localhost:${PORT} ***`);
 });
