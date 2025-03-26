d932cf1f517bc5c08b83af45ec401fff755b1c24
Alex Handley <smithvillescholarsah@gmail.com>
2025-03-26 14:33:42 -0500

    starting scripts

---
+++ public/scripts/script.js
@@ -0,0 +1,5 @@
+//// This script is used for the certificate images on the Home page ////
+
+const c1 = document.getElementById("c1").addEventListener("click", (event) => {
+    console.log("Image 1");
+});
