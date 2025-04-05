c5aab404949ddaefdad4377e4cab300acb2ebaa0
Alex Handley <smithvillescholarsah@gmail.com>
2025-04-05 15:05:03 -0500

    image buttons wip

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,15 +3,35 @@
 const c1 = document.getElementById("c1").addEventListener("click", (event) => {
     console.log("Image 1");
 });
-
 const c2 = document.getElementById("c2").addEventListener("click", (event) => {
     console.log("Image 2");
 });
 
-const left_Button = document.getElementById("left-button").addEventListener("click", (event) => {
-    console.log("you cliked ⇦! this way!");
-});
+const left_Button = document.getElementById("left-button");
+const right_Button = document.getElementById("right-button");
 
-const right_Button = document.getElementById("right-button").addEventListener("click", (event) => {
-    console.log("⇨ that way!");
-});
+function leftB_Function(event) {
+    if (event.type === "click") {
+        console.log("Left Button clicked!");
+    } else if (event.type === "keydown") {
+        if (event.key === "ArrowLeft") {
+            console.log("Left Arrow pressed!");  
+        }
+    }
+}
+
+left_Button.addEventListener("click", leftB_Function);
+left_Button.addEventListener("keydown", leftB_Function);
+
+function rightB_Function(event) {
+    if (event.type === "click") {
+        console.log("Right Button clicked!");
+    } else if (event.type === "keydown") {
+        if (event.key === "ArrowRight") {
+            console.log("Right Arrow pressed!");  
+        }
+    }
+}
+
+right_Button.addEventListener("click", rightB_Function);
+right_Button.addEventListener("keydown", rightB_Function);
