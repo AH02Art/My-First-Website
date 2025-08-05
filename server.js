85ba4866cf7ee5558bece9d755cfef684dba1bb5
Alex Handley <smithvillescholarsah@gmail.com>
2025-08-05 12:21:19 -0500

    Version 1.0 complete! :D

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 // server dependencies
 const express = require("express");
 const path = require("path");
-// These dependencies are to use Live Server + nodemon together
-const livereload = require("livereload");
-const connectLivereload = require("connect-livereload");
-
 const app = express();
-const PORT = 5500;
+const PORT = process.env.PORT || 5500;
 
-// what specified folders to monitor changes
-const liveReloadServer = livereload.createServer();
-liveReloadServer.watch([
-    path.join(__dirname, "public"),
-    path.join(__dirname, "views")
-]);
+if (process.env.NODE_ENV !== "production") {
+    // These dependencies are to use Live Server + nodemon together
+    const livereload = require("livereload");
+    const connectLivereload = require("connect-livereload");
+    // what specified folders to monitor changes
+    const liveReloadServer = livereload.createServer();
+    liveReloadServer.watch([
+        path.join(__dirname, "public"),
+        path.join(__dirname, "views")
+    ]);
+    // a Timer for when the server resets after changes (I may change it...)
+    liveReloadServer.server.once("connection", function() {
+        console.log("LiveReload online");
+        setTimeout(function() {
+            console.log("Browser refreshed...");
+            liveReloadServer.refresh("/");
+        }, 100);
+    });
+    app.use(connectLivereload());
+}
 
-// a Timer for when the server resets after changes (I may change it...)
-liveReloadServer.server.once("connection", function() {
-    console.log("LiveReload online");
-    setTimeout(function() {
-        console.log("Browser refreshed...");
-        liveReloadServer.refresh("/");
-    }, 100);
-});
-
-app.use(connectLivereload());
 app.use(express.static(path.join(__dirname, "public")));
 
 // setting ejs as the template engine
